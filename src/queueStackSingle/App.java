74462fe0f7f050bfa2ecd14f4d9161992612e15d
DAVID <dpunzel15@georgefox.edu>
2019-10-20 23:04:41 -0700

    Single stack solution for Queue stack

---
+++ src/queueStackSingle/App.java
@@ -0,0 +1,4 @@
+package queueStackSingle;
+
+public class App {
+}
---
+++ src/queueStackSingle/QueueStackSingle.java
@@ -0,0 +1,38 @@
+package queueStackSingle;
+
+import java.util.Stack;
+
+/**
+ * A single stack queue
+ */
+public class QueueStackSingle {
+    // one stack for enqueue operation
+    private Stack<Integer> stack;
+
+    public QueueStackSingle() {
+        this.stack = new Stack<>();
+    }
+
+    // method to add item to queue O(1) time
+    public void enqueue(int item) {
+        this.stack.push(item);
+    }
+
+    // recursive call utilizing the call stack
+    public int dequeue() {
+        // base case if stack FIFO item found
+        if (stack.size() == 1) {
+            return stack.pop();
+        }
+
+        // pop items until last is found
+        int item = stack.pop();
+
+        // calling recursively to find
+        int lastItem = dequeue();
+
+        enqueue(item);
+
+        return lastItem;
+    }
+}
